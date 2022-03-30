5aacab4a45a7ffacd8dff3fb77406cfba905ce3c
awanish mishra <awanish.mishra@gingerwebs.co.in>
2022-03-30 21:37:29 +0530

    new project

---
+++ reduxapp/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Sub from './Sub';
+function App() {
+  return (
+    <>
+    <h1> the react app</h1>
+
+<Sub dataa={{'a':'awani',2:'mishra',3:"dehariya"}}/>
+    </>
+  );
+}
+
+export default App;
---
+++ reduxapp/my-app/src/Sub.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Sub(props) {
+    const {dataa} = props
+  return (
+    <div>{dataa.a}</div>
+  )
+}
+
+export default Sub
